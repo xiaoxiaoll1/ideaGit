9910c006280a79242967f405affb51868eb6138d
xiao <904886481@qq.com>
2020-08-24 02:31:37 +0800

    first git idea

---
+++ src/com/xiao/FirstGit.java
@@ -0,0 +1,11 @@
+package com.xiao;
+
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("xioase");
+        System.out.println("xioase");
+        System.out.println("xioase");
+        System.out.println("xioase");
+    }
+}
